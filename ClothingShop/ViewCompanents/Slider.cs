bc22b01433d475ca8bd037c93c26717ef869abc2
IlkinNovruzov <inovruzov2004@gmail.com>
2024-01-26 16:46:49 +0400

    Update

---
+++ ClothingShop/ViewCompanents/Slider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClothingShop.ViewCompanents
+{
+    public class Slider:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
